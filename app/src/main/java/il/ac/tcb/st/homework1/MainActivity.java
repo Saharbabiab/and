7ec8b61bfced0595b7c7faeb06db4aae1c100ae0
Saharbabiab <Saharbabian@gmail.com>
2024-05-09 11:09:13 +0300

    s

--- app/src/main/java/il/ac/tcb/st/homework1/MainActivity.java
+++ app/src/main/java/il/ac/tcb/st/homework1/MainActivity.java
@@ -1,6 +1,11 @@
 package il.ac.tcb.st.homework1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textViewLike;
+    EditText editTextUsername;
+    EditText editTextPassword;
+    Button buttonLogin;
+    CheckBox checkBoxRememberme;
+
+    private int nTimesLiked=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +32,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textViewLike=findViewById(R.id.textViewLike);
+        editTextUsername=findViewById(R.id.editTextUsername);
+        editTextPassword=findViewById(R.id.editTextPassword);
+        buttonLogin=findViewById(R.id.buttonLogin);
+        checkBoxRememberme=findViewById(R.id.checkBoxRememberme);
+
+        textViewLike.setOnClickListener(textViewLikeOnClickListener());
+        buttonLogin.setOnClickListener(buttonLoginOnClickListener());
+    }
+
+    View.OnClickListener buttonLoginOnClickListener(){
+        return v-> {
+            ()
+        };
+    }
+
+    View.OnClickListener textViewLikeOnClickListener(){
+        return v->{
+            nTimesLiked++;
+        };
     }
 }
